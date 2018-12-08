53e16bfa4111738969de2344c7e1f7e8b365dab5
hiddenwarrior <manfscience@gmail.com>
2018-12-08 18:15:37 +0200

    rest_api: finish testing

--- src/test/scala/restaurant/api/apiSpec.scala
+++ src/test/scala/restaurant/api/apiSpec.scala
@@ -9,37 +9,66 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{ Matchers, WordSpec }
 import akka.util.ByteString
+import restaurant.data.data.Restaurant
+
+
+import spray.json.DefaultJsonProtocol._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
 
 class RestaurantRoutesSpec extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest
     with RestaurantRoute {
+    val uuid = "5dc94cbf-add9-11e7-b988-0242ac110002"
+    val uuid2 = "5dc94e49-add9-11e7-b988-0242ac110002"
     val jsonRequest = ByteString(
         """
-        {
-        "uuid": "5d81a479-add9-11e7-b988-0242ac10002",
+            {
+        "uuid": "5dc94cbf-add9-11e7-b988-0242ac110002",
         "data": {
-            "enName": "3 Ahwa Cafe",
-            "arName": "عالقهوة كافيه",
+            "enName": "After Eight(Closed)",
+            "arName": "أفتر ايت(مغلق)",
+            "state": "UNPUBLISHED",
+            "routingMethod": null,
+            "logo": "ig22v040ud4h1tt9.jpg",
+            "coverPhoto": null,
+            "enDescription": "",
+            "arDescription": null,
+            "shortNumber": "11361",
+            "facebookLink": "",
+            "twitterLink": "",
+            "youtubeLink": "",
+            "website": null,
+            "onlinePayment": false,
+            "client": false,
+            "pendingInfo": true,
+            "pendingMenu": true,
+            "closed": true
+        }
+    }
+        """.stripMargin)
+     val jsonRequest2 = ByteString("""{
+        "uuid": "5dc94e49-add9-11e7-b988-0242ac110002",
+        "data": {
+            "enName": "Spago",
+            "arName": "سباجو",
             "state": "PUBLISHED",
             "routingMethod": null,
-            "logo": "i3qf6gym1p833di.jpg",
-            "coverPhoto": "",
+            "logo": "e9fo4fb8.jpg",
+            "coverPhoto": "irotwr8uyn0l766r.jpg",
             "enDescription": "",
-            "arDescription": "",
+            "arDescription": null,
             "shortNumber": "",
             "facebookLink": "",
             "twitterLink": "",
             "youtubeLink": "",
-            "website": "",
-            "onlinePayment": true,
+            "website": null,
+            "onlinePayment": false,
             "client": false,
             "pendingInfo": true,
             "pendingMenu": true,
             "closed": false
-            }
         }
-        """.stripMargin)
- 
+    }""".stripMargin)
 
   "RestaurantRoutes" should {
         "return empty list if no restaurants were added" in {
@@ -59,7 +88,7 @@ class RestaurantRoutesSpec extends WordSpec with Matchers with ScalaFutures with
             
             val request = HttpRequest(
                 HttpMethods.POST,
-                uri = "/api/restaurants",
+                uri = "/api/restaurant",
                 entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
 
             request ~> route ~> check {
@@ -67,5 +96,87 @@ class RestaurantRoutesSpec extends WordSpec with Matchers with ScalaFutures with
                     entityAs[String] should ===("""done""")
             }
         }
+        "prevent adding duplicate restaurants (POST /api/restaurants)" in {
+            
+            val request = HttpRequest(
+                HttpMethods.POST,
+                uri = "/api/restaurant",
+                entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+
+            request ~> route
+            request ~> route ~> check {
+                status should ===(StatusCodes.AlreadyReported)
+            }
+
+
+        }
+
+        "updating (PUT /api/restaurants/uuid)" in {
+            
+            val addRequest = HttpRequest(
+                HttpMethods.POST,
+                uri = "/api/restaurant",
+                entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+
+            addRequest ~> route
+            val updateRequest = HttpRequest(
+                HttpMethods.PUT,
+                uri = "/api/restaurant/"+uuid,
+                entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+
+            updateRequest ~> route ~> check {
+                 status should ===(StatusCodes.OK)
+            }
+
+
+        }
+
+        "prevent request from updating not found object (PUT /api/restaurants/uuid)" in {
+            
+            val addRequest = HttpRequest(
+                HttpMethods.POST,
+                uri = "/api/restaurant",
+                entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+
+            addRequest ~> route
+            val updateRequest = HttpRequest(
+                HttpMethods.PUT,
+                uri = "/api/restaurant/"+uuid2,
+                entity = HttpEntity(MediaTypes.`application/json`, jsonRequest2))
+            println("/api/restaurant/"+uuid2)
+            updateRequest ~> route ~> check {
+                 status should !==(StatusCodes.OK)
+            }
+
+
+        }
+        "getting closed and opened restaurants only (GET /api/restaurants)" in {
+            
+            val request1 = HttpRequest(
+                HttpMethods.POST,
+                uri = "/api/restaurant",
+                entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+
+            val request2 = HttpRequest(
+                HttpMethods.POST,
+                uri = "/api/restaurant",
+                entity = HttpEntity(MediaTypes.`application/json`, jsonRequest2))
+
+            request1 ~> route
+            request2 ~> route
+            HttpRequest(uri = "/api/restaurant?closed=0") ~> route ~> check{
+                entityAs[ List[Restaurant] ].length should ===(1)
+            }
+            HttpRequest(uri = "/api/restaurant?closed=1") ~> route ~> check{
+                entityAs[ List[Restaurant] ].length should ===(1)
+            }
+
+            HttpRequest(uri = "/api/restaurant") ~> route ~> check{
+                entityAs[ List[Restaurant] ].length should ===(2)
+            }
+
+        }
+
+
     }
 }
